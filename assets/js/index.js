4594a6c0e512192371fe09ab50d30a1a99d3f7d4
arishorts <arieljschwartz@gmail.com>
2023-01-16 23:29:33 -0700

    Bugix where resetting the LocalStorage would cause parseSchedule to null and spiral into storage errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,24 @@
-var obj = { "09 AM": "", "10 AM": "", "11 AM": "", "12 PM": "" };
+var obj = {
+  "9 AM": "",
+  "10 AM": "",
+  "11 AM": "",
+  "12 PM": "",
+  "1 PM": "",
+  "2 PM": "",
+  "3 PM": "",
+  "4 PM": "",
+  "5 PM": "",
+};
 
 //parse object from localstorage with included verification step
 var jsonString = localStorage.getItem("storageSchedule");
-if (typeof jsonString !== "undefined") {
+if (jsonString) {
   var parseSchedule = JSON.parse(jsonString);
+  //set obj equal to all key-value pairs in the parsed schedule
+  obj = parseSchedule;
 } else {
   console.log("myVariable is undefined");
 }
-console.log(parseSchedule);
-//set obj equal to all key-value pairs in the parsed schedule
-obj = parseSchedule;
 
 //set the localstorage values for obj to the corresponding textareas at specified times
 $(function () {

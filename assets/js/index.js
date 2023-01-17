aa7dd22587142a943e709332f39150b3a52871a6
arishorts <arieljschwartz@gmail.com>
2023-01-17 12:27:40 -0700

    Added js to calculate the time of day using day.js then alter a textareas background to reflect the time now. Minor additional updates to css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+//https://stackoverflow.com/questions/61339968/error-message-devtools-failed-to-load-sourcemap-could-not-load-content-for-chr
+//https://www.udemy.com/course/jquery-tutorial/learn/lecture/4968932#notes
+//https://day.js.org/en/
+
 var obj = {
   "9 AM": "",
   "10 AM": "",
@@ -20,8 +24,8 @@ if (jsonString) {
   console.log("myVariable is undefined");
 }
 
-//set the localstorage values for obj to the corresponding textareas at specified times
-$(function () {
+//set the localstorage values for obj to the corresponding textareas at specified times when document has fully loaded
+$(document).ready(function () {
   for (let key in parseSchedule) {
     $("h3").each(function (index) {
       if (key === $(this).text()) {
@@ -37,10 +41,25 @@ $(function () {
   }
 });
 
+//find the time of day
+$(function () {
+  const now = dayjs();
+  let specifiedText = now.format("h A");
+  let divs = $(".block__hour");
+  let matchingDivs = divs.filter(function () {
+    return $(this).text() === specifiedText;
+  });
+  matchingDivs.parent().find("textarea").css({ "background-color": "grey" });
+});
+
 $(function () {
   //save events that we want in localstorage (variable set to storageSchedule)
   $("a").on("click", function (event) {
     event.preventDefault();
+    $(".lSUpdate").css({ visibility: "visible" });
+    setTimeout(function () {
+      $(".lSUpdate").stop().css({ visibility: "hidden" });
+    }, 1000);
     var hour = $(this).parent().find("h3").text();
     var textarea = $(this).parent().find("textarea").val();
     obj[hour] = textarea;
@@ -49,3 +68,41 @@ $(function () {
     localStorage.setItem("storageSchedule", stringifySchedule);
   });
 });
+
+// var targetDay = dayjs("2027-05-04");
+
+// // Use Day.js to format the date and assign to the declared variable.
+
+// // TODO: 1. What is your graduation date in the following format: Jan 1, 1999?
+// var gradDate = dayjs("2022-08-01").format("MMM D, YYYY");
+// $("#1a").text(gradDate);
+
+// // TODO: 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
+// var weekDay = dayjs("2027-01-01").format("dddd");
+// $("#2a").text(weekDay);
+
+// // TODO: 3. What is the current time in the format: hours:minutes:seconds
+// var time = dayjs().format("h:mm:ss");
+// $("#3a").text(time);
+
+// // TODO: 4. What is the current Unix timestamp?
+// var unix = dayjs().unix();
+// $("#4a").text(unix);
+
+// // TODO: 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
+// var unixFormat = dayjs.unix(1318781876).format("M/D/YYYY h:mm:ss");
+// $("#5a").text(unixFormat);
+
+// // TODO: 6. What is the difference in days between May 4, 2027 and today? Hint:
+// // You can display the difference between two dayjs objects by using the dayjs
+// // diff method.)
+
+// // dayjs object for May 4, 2027
+// var targetDay = dayjs("2027-05-04");
+
+// // dayjs object for today
+// var today = dayjs();
+
+// // number of days between targetDay and today
+// var days = targetDay.diff(today, "days");
+// $("#6a").text(days);

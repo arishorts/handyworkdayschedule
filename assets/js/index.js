1c5f12198c99117a39e178831fa5d03fc2a31ad9
arishorts <arieljschwartz@gmail.com>
2023-01-17 17:07:06 -0700

    Added current date at top of the page. Still requires a bugfix with advanced formatting for the ordinal

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,14 +41,18 @@ $(document).ready(function () {
   }
 });
 
-//find the time of day
+//find the times of day
 $(function () {
+  //bugfix : need advanced format plugin to add ordinal
+  //https://day.js.org/docs/en/plugin/advanced-format
+  const headerNow = dayjs().format("dddd, MMM Do YYYY");
+  $("h2:last").text(headerNow);
   // dayjs($("9 AM", "h A").isBefore(dayjs()));
 
   // console.log(dayjs($("3 PM", "h A"), "h A").isBefore(dayjs()));
   const now = dayjs();
-  let time1 = dayjs("2023-01-17T13:00:00"); // 1 PM
-  let time2 = dayjs("2023-01-17T19 PM"); // 3 PM
+  // let time1 = dayjs("2023-01-17T13:00:00"); // 1 PM
+  // let time2 = dayjs("2023-01-17T19 PM"); // 3 PM
   // console.log(now.isBefore(time2));
 
   // console.log(now.isBefore(dayjs("2023-01-17T12 PM")));
@@ -123,41 +127,3 @@ $(function () {
     localStorage.setItem("storageSchedule", stringifySchedule);
   });
 });
-
-// var targetDay = dayjs("2027-05-04");
-
-// // Use Day.js to format the date and assign to the declared variable.
-
-// // TODO: 1. What is your graduation date in the following format: Jan 1, 1999?
-// var gradDate = dayjs("2022-08-01").format("MMM D, YYYY");
-// $("#1a").text(gradDate);
-
-// // TODO: 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
-// var weekDay = dayjs("2027-01-01").format("dddd");
-// $("#2a").text(weekDay);
-
-// // TODO: 3. What is the current time in the format: hours:minutes:seconds
-// var time = dayjs().format("h:mm:ss");
-// $("#3a").text(time);
-
-// // TODO: 4. What is the current Unix timestamp?
-// var unix = dayjs().unix();
-// $("#4a").text(unix);
-
-// // TODO: 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
-// var unixFormat = dayjs.unix(1318781876).format("M/D/YYYY h:mm:ss");
-// $("#5a").text(unixFormat);
-
-// // TODO: 6. What is the difference in days between May 4, 2027 and today? Hint:
-// // You can display the difference between two dayjs objects by using the dayjs
-// // diff method.)
-
-// // dayjs object for May 4, 2027
-// var targetDay = dayjs("2027-05-04");
-
-// // dayjs object for today
-// var today = dayjs();
-
-// // number of days between targetDay and today
-// var days = targetDay.diff(today, "days");
-// $("#6a").text(days);
